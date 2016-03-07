44020798d513d90b747da49cd6b240b430305a89
Mikhail Voloshin <mvol@live.com>
2016-03-07 14:03:04 -0500

    I appear to be correctly loading the entry point.

--- tads3.js
+++ tads3.js
@@ -10,12 +10,27 @@
 var tads3 = function(t3binarydata, divselector) {};
 
 (function() { // Master IIFE
+  var CONSTANTS = {
+    // A string that must be found in the file header.
+    // Marks the beginning of readable data.
+    VMIMAGE_SIG: "T3-image\015\012\032",
 
+    // A flag found at the beginning of each data block.
+    // If set to true, the block is mandatory to read, and
+    // if the parser cannot process it then it must abort.
+    VMIMAGE_DBF_MANDATORY: 0x0001,
+  };
+
+  var VM_DATA = {
+    // An integer representing the offset in the VM code that
+    // represents the program's entry point.
+    entry_point: null
+  };
+  
+  
   var parse;
 
   (function() { // Parser IIFE
-    var VMIMAGE_SIG = "T3-image\015\012\032";
-  
     var _curpos = 0;
     var _data = null;
 
@@ -46,20 +61,31 @@ var tads3 = function(t3binarydata, divselector) {};
       return padding;
     };
     
-    var readInt16 = function() {
-      // t3 bytecode is little-endian.
-      var bytes = readBytes(2);
-      return bytes.charCodeAt(0) + (bytes.charCodeAt(1) << 8);
-    }
+    var toLittleEndianNumber = function(s) {
+      var retval = 0;
+      for (i = 0; i < s.length; i++) {
+        retval += s.charCodeAt(i) << (i * 8);
+      }
+      return retval;
+    };
+    
+    
+    var readUint16 = function() {
+      return toLittleEndianNumber(readBytes(2));
+    };
+
+    var readUint32 = function() {
+      return toLittleEndianNumber(readBytes(4));
+    };
 
 
-    var readFileHeader = function(data, bytepos) {
-      var fileSig = readBytes(VMIMAGE_SIG.length);
-      if (fileSig !== VMIMAGE_SIG) {
+    var readFileHeader = function() {
+      var fileSig = readBytes(CONSTANTS.VMIMAGE_SIG.length);
+      if (fileSig !== CONSTANTS.VMIMAGE_SIG) {
         throw new TypeError('TADS3 parse error: file signature invalid.', null, _curpos);
       }
       
-      var fileVersionNum = readInt16();
+      var fileVersionNum = readUint16();
 
       if (fileVersionNum !== 1) {
         throw new TypeError('TADS3 parse error: does not support version ' + 
@@ -71,17 +97,91 @@ var tads3 = function(t3binarydata, divselector) {};
       var fileTimestamp = readBytes(24);
       console.log(fileTimestamp);
     };
+
+    var readDataBlock = function() {
+      var blockTypeCode = readBytes(4).trim();
+      var blockSize = readUint32();
+      var flags = readUint16();
+      
+      var moreDataToRead = true;
+      
+      switch (blockTypeCode) {
+        case "EOF":
+          moreDataToRead = false;
+          break;
+
+        case "ENTP":
+          readEntryPointDataBlock(blockSize);
+          break;
+          
+        case "CPPG":
+        case "OBJS":
+        case "CPDF":
+        case "MRES":
+        case "MREL":
+        case "MCLD":
+        case "FNSD":
+        case "SYMD":
+        case "SRCF":
+        case "GSYM":
+        case "MACR":
+        case "MHLS":
+        case "SINI":
+        default: {
+          var isMandatory = flags & CONSTANTS.VMIMAGE_DBF_MANDATORY;
+          isMandatory = false; // TODO: REMOVE THIS!!! DEVELOPMENT PURPOSES ONLY!!
+          if (isMandatory) {
+            throw new TypeError('TADS3 parse error: unknown data block type: ' + 
+                blockTypeCode, null, _curpos);
+          } else {
+            console.log('TADS3 parse warning: unknown data block type: ' + blockTypeCode);
+            readNullPadding(blockSize, true);
+          }
+        }
+      }
+
+      return moreDataToRead;
+    };
+
+    var readEntryPointDataBlock = function(blockSize) {
+      if (!!VM_DATA.entry_point) {
+        throw new TypeError('TADS3 parse error: Entry point already set', 
+            null, _curpos);
+      }
+
+      VM_DATA.entry_point = readUint32();
+      VM_DATA.function_header_size = readUint16();
+      VM_DATA.exception_table_entry_size = readUint16();
+      VM_DATA.debugger_source_line_record_size = readUint16();
+      VM_DATA.debug_table_header_size = readUint16();
+      VM_DATA.debug_local_symbol_header_size = readUint16();
+      VM_DATA.debug_format_version_id = readUint16();
+
+      VM_DATA.debug_frame_size = 4;
+      blockSize -= 16;
+      if (blockSize >= 2) {
+        VM_DATA.debug_frame_size = readUint16();
+        blockSize -= 2;
+      }
+      
+      readNullPadding(blockSize, true);
+    };
+    
     
     parse = function(data) {
       _data = data;
+      _curpos = 0;
       
-      readFileHeader(data, 0);
+      readFileHeader();
+      while (readDataBlock());
     };
     
   }()); // Close parser IIFE
 
   tads3 = function(t3binarydata, divselector) {
     parse(t3binarydata);
+    
+    window.VM_DATA = VM_DATA;
   };
 
 }()); // Close the master IIFE
